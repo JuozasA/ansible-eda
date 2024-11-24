630e764f9eb841b50b4e366649460fb8b34c334f
Juozas Auskalnis <j.auskalnis@gmail.com>
2024-11-24 18:54:51 +0000

    Update business_rule.js

--- business_rule/business_rule.js
+++ business_rule/business_rule.js
@@ -1,7 +1,7 @@
 (function executeRule(current, previous /*null when async*/ ) {
     try {
         var credentialsProvider = new sn_cc.StandardCredentialsProvider();
-        var credential = credentialsProvider.getCredentialByID('20a6268dc3ca16101966de1d05013151');
+        var credential = credentialsProvider.getCredentialByID('20a6268dc3ca16101966de1d05013152');
         var token = credential.getAttribute('password');
         if (!token) {
             gs.error("Failed to retrieve API token from credentials.");
@@ -10,15 +10,16 @@
 
         var r = new sn_ws.RESTMessageV2();
         // Enter EDA URL and port number here. In this one we have eda.example.com port 5000.
-        r.setEndpoint("http://aap-eda.2hnbk.gcp.redhatworkshops.io:5000/endpoint");
+        r.setEndpoint("http://ec2-18-170-28-29.eu-west-2.compute.amazonaws.com:5000/endpoint");
         r.setHttpMethod("post");
         r.setRequestHeader("Authorization", "Bearer " + token);
         r.setRequestHeader("Content-Type", "application/json");
 
         // Get data from Incident record
-	var ci = new GlideRecord('cmdb_ci');
-	ci.get('sys_id', current.getValue("cmdb_ci"));
-	var cmdb_ci = ci.getValue('name');
+
+        var ci = new GlideRecord('cmdb_ci');
+        ci.get('sys_id', current.getValue("cmdb_ci"));
+        var cmdb_ci = ci.getValue('name');
         var number = current.getValue("number");
         var short_description = current.getValue("short_description");
 
